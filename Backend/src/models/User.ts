18a74c7ea3898ff704b8d439a5c6fe68589cb415
Fahad MB <fahadmbasheer@gmail.com>
2026-08-25 12:31:05 +0530

    feat: add ModelUser

---
+++ Backend/src/models/User.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
