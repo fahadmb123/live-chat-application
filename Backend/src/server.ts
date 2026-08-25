f7626b5c5bf61d12aa06de89e8655cb7dd14dde8
Fahad MB <fahadmbasheer@gmail.com>
2026-08-25 15:02:46 +0530

    configure websocket for vercel

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,26 +1,29 @@
+import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connection.js";
-import cron from "node-cron"
+import cron from "node-cron";
 import { Message } from "./models/Message.js";
-import "./websocket.js"
 
+dotenv.config()
 
+const app = express()
 
+app.use(express.json())
 
-const dns = require("dns")
-console.log(dns.getServers());
-dns.setServers(["8.8.8.8", "8.8.4.4"])
-
-dotenv.config();
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Backend is running",
+  })
+})
 
 const startServer = async () => {
-  await connectDB();
+  await connectDB()
 
-  console.log("Backend started");
+  console.log("Backend started")
 };
 
 startServer()
-
 
 cron.schedule("0 0 * * *", async () => {
   try {
@@ -31,3 +34,5 @@ cron.schedule("0 0 * * *", async () => {
     console.error("Error clearing messages:", error);
   }
 })
+
+export default app;
