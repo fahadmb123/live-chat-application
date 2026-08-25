27acd6456bc1fe1dec727c579ed6ef5664e0ed54
Fahad MB <fahadmbasheer@gmail.com>
2026-08-25 15:30:24 +0530

    feat:add DNS

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -8,6 +8,13 @@ dotenv.config()
 
 const app = express()
 
+
+const dns = require("dns")
+console.log(dns.getServers());
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+
+
 app.use(express.json())
 
 app.get("/", (req, res) => {
