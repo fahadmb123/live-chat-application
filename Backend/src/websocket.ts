5216b092e31aa6991bba0921aca2c2af6ee947a0
Fahad MB <fahadmbasheer@gmail.com>
2026-08-25 11:43:51 +0530

    feat:Bck Initialize ws

---
+++ Backend/src/websocket.ts
@@ -0,0 +1,29 @@
+import { WebSocketServer } from "ws";
+
+
+const wss = new WebSocketServer({
+    port : 8080
+})
+
+wss.on("connection",(socket)=>{
+    console.log("User Connected")
+
+    socket.on("message",(message)=>{
+        const data = JSON.parse(message.toString())
+        console.log("Message",data.message)
+
+
+        wss.clients.forEach((user)=>{
+            if (user.readyState === 1){
+                user.send(JSON.stringify({
+                    message:data.message
+                }))
+            }
+        })
+    })
+
+
+    socket.on('close',()=>{
+        console.log("User Disconnected")
+    })
+})
