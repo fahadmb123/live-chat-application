b79f76b3b1ae9f5b4195c41203b2721cc80afbfe
Fahad MB <fahadmbasheer@gmail.com>
2026-08-25 12:40:39 +0530

    feat:add server

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,2 +1,13 @@
-import "./websocket"
-console.log("Backend is Running")
+import dotenv from "dotenv";
+import { connectDB } from "./db/connection.js";
+import "./websocket.js";
+
+dotenv.config();
+
+const startServer = async () => {
+  await connectDB();
+
+  console.log("Backend started");
+};
+
+startServer()
