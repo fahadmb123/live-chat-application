a64082fdb5b694a3207ad8ae1fc714e2d7b7ede5
Fahad MB <fahadmbasheer@gmail.com>
2026-08-25 19:54:26 +0530

    chore:update server

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -6,6 +6,13 @@ import { connectDB } from "./db/connection";
 dotenv.config();
 
 const app = express();
+
+const dns = require("dns")
+console.log(dns.getServers());
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
