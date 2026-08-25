0f3b196be096edbae2b69e8a14cae84ed0f5c52d
Fahad MB <fahadmbasheer@gmail.com>
2026-08-25 21:15:04 +0530

    Server Update CORS

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -17,7 +17,12 @@ const PORT = Number(process.env.PORT) || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://live-chat-application-spqn.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
   })
 );
 
